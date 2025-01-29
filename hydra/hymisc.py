eeeda573002afe0d54d8a9da79806e7d419c7486
DragonDelgar <delgarex@gmail.com>
2025-01-29 16:25:05 -0500

    Version number

--- hydra/hymisc.py
+++ hydra/hymisc.py
@@ -12,7 +12,7 @@ Records with an old major version will be wiped without trying to read them.
 Records with an old minor version will be marked stale, but will work normally.
 
 """
-HYDRA_VERSION = (0,1,1)
+HYDRA_VERSION = (0,1,2)
 
 
 """Static paths and files"""
